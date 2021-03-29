49b85a40fdb003874a86aa07b02658e724e82788
Ronaldo Borges <rpborges@gmail.com>
2021-03-29 16:44:54 -0300

    retirada acentuacao de nome de arquivo e incluida linhas de comentarios

---
+++ comentario.js
@@ -0,0 +1,19 @@
+// comentário de uma linha
+console.log('linha 1')
+
+/*
+comentários de 
+multiplas linhas
+*/
+console.log('linha 2')
+
+/*
+ * 
+comentario de
+ * multiplas linhas
+ *
+ * 
+ * 
+ */
+
+console.log('linha 3')
